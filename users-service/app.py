fa73d246a52d4699d805fa5e35caef4e2b4d2f86
Kaviya <parthibankaviya912@gmail.com>
2026-03-09 02:59:04 +0530

    Fix folder name to users-service

---
+++ users-service/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+users = [
+    {"id": 1, "name": "Alice"},
+    {"id": 2, "name": "Bob"}
+]
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    return jsonify(users)
+
+@app.route('/users', methods=['POST'])
+def add_user():
+    data = request.get_json()
+    new_id = max(user["id"] for user in users) + 1
+    new_user = {"id": new_id, "name": data["name"]}
+    users.append(new_user)
+    return jsonify(new_user), 201
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
